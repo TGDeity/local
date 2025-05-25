37538d4b38b26973a948779ed673157a9a291d43
Tolga Durak <tolga.durak@ad.codec.com.tr>
2025-05-25 21:45:28 +0300

    Signed-off-by: Tolga Durak <tolga.durak@ad.codec.com.tr>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: unused_import
+import 'dart:io';
+
 import 'package:ders/101/app_bar.learn.dart';
 import 'package:ders/101/button_learn.dart';
 import 'package:ders/101/card_learn.dart';
@@ -19,8 +21,17 @@ import 'package:ders/101/text_learn.dart';
 import 'package:ders/demos/notes_example_view.dart';
 import 'package:ders/demos/stack_demo_view.dart';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Flutter Windows App'); // Pencere başlığını ayarla
+    setWindowMinSize(const Size(360, 640)); // Minimum boyutu telefon boyutuna ayarla
+    setWindowMaxSize(const Size(360, 640)); // Maksimum boyutu da aynı yaparak sabitle
+    setWindowFrame(const Rect.fromLTWH(0, 0, 360, 640)); // Başlangıç boyutu (genişlik, yükseklik)
+  }
   runApp(const MyApp());
 }
 
@@ -29,6 +40,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
